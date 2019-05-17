808e0328f5d16df986fd998e1f8a75bf8b31ab05
KayleeHartzog <har15031@byui.edu>
2019-05-17 16:56:26 -0600

    did LYN4.js

---
+++ LYN4.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var count = 0;
+
+function getInFile(callback) {
+    fs.readFile(process.argv[2], 'utf8' ,function doneReading(err, fileContents) {
+        for (let i = 0; i < fileContents.length; i++) {
+            if (fileContents[i] == "\n") {
+                count++;
+            }
+        }
+        callback()
+    });
+
+}
+//var inFile = fs.readFile(process.argv[2]).toString();
+function displayCount() {
+    console.log(count)
+}
+
+getInFile(displayCount)
+
