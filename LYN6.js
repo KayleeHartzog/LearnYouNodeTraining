240f4ab91b59ab641b3bda7ac759a7281f9dafc7
KayleeHartzog <har15031@byui.edu>
2019-05-21 19:34:58 -0600

    finished LYN6.js

--- LYN6.js
+++ LYN6.js
@@ -1,3 +1,13 @@
-var module = require('./myModule.js')
+var myModule = require('./myModule.js')
+// let fileArray = []
+let myDirectory = process.argv[2]
+let extension = process.argv[3]
 
-module.getInFile()
+
+function displayFolders(arg1, arg2) {
+    arg2.forEach(function(file) {
+        console.log(file)
+    })
+}
+
+myModule(myDirectory, extension, displayFolders)
--- myModule.js
+++ myModule.js
@@ -1,26 +1,46 @@
 var fs = require('fs')
 let fileArray = []
 
-// var exports = module.exports = {}
 
-exports.getInFile(callback) = function {
-    fs.readdir(process.argv[2], 'utf8' ,function doneReading(err, fileContents) {
+function getInFile(directory, extension, callback) {
+    fs.readdir(directory, 'utf8' ,function doneReading(err, fileContents) {
+        if (err){
+            // console.log("did I get an error?")
+            return callback(err)
+        }
+        // console.log("I got in here")
         let tempArray = fileContents
-        let temp = "." + process.argv[3]
-       // console.log(temp)
+        let temp = "." + extension
+        // console.log(temp)
         fileArray = tempArray.filter(file => file.includes(temp))
-      //  console.log(fileArray)
-        callback()
+        // console.log(fileArray)
+        return callback(null, fileArray)
     });
 
 }
 
+module.exports = getInFile;
+
+// module.exports.getInFile = function (callback) {
+//     fs.readdir(process.argv[2], 'utf8' ,function doneReading(err, fileContents) {
+//         let tempArray = fileContents
+//         let temp = "." + process.argv[3]
+//        // console.log(temp)
+//         fileArray = tempArray.filter(file => file.includes(temp))
+//       //  console.log(fileArray)
+//         callback()
+//     });
+
+// }
+
 // module.exports = getInFile(process.argv[2], process.argv[3], displayFolders())
 
-function displayFolders() {
-    fileArray.forEach(function(file) {
-        console.log(file)
-    })
-}
+// function displayFolders() {
+//     fileArray.forEach(function(file) {
+//         console.log(file)
+//     })
+// }
 
-getInFile(displayFolders)
+// module.exports.myCallback = displayFolders;
+
+// getInFile(displayFolders)
